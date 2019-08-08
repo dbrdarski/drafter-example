2abd533c9b553beb20cbd9f8a3a227aac552c1ad
Dane Brdarski <dane.brdarski@gmail.com>
2019-08-08 22:59:38 +0200

    experimented with some new concepts

--- src/index2.js
+++ src/index2.js
@@ -108,3 +108,147 @@ const componentExample = ({ createState } = {}) => {
 
 // console.log(componentExample());
 window.vdom = renderNode(componentExample())(document.body);
+
+const example2 = ({ attrs, createState }) => {
+  const state = createState({ someState:  });
+  const user = connectStore(Auth);
+  return (
+    <div dynamicAttr={ () => attrs.dynamicAttr }>
+      <div class="static">Hello, {() => state.user.name }</div>
+      { () => state.user.hasSubscribers && (
+        <div class="subscribers">
+          Whatever, { () => state.user.name }
+        </div>
+      ) }
+      { () => state.someCondition
+        ? <Component1 user={state.user} />
+        : <Component2 user={state.user} />
+      }
+    </div>
+  )
+}
+
+const example3 = ({ attrs, createState }) => {
+  const state = createState({ someState:  });
+  const user = connectStore(Auth);
+  return (
+    <div dynamicAttr={ () => attrs.dynamicAttr }>
+      <div class="static">Hello, {() => state.user.name }</div>
+      <GUARD if={state.user.hasSubscribers}>
+        <div class="subscribers">
+          Whatever, { () => state.user.name }
+        </div>
+      </GUARD>
+      <IF>
+        ) }
+      { () => state.someCondition
+        ? <Component1 user={state.user} />
+        : <Component2 user={state.user} />
+      }
+      </IF>
+    </div>
+  )
+}
+
+
+const example4 = ({ createState, connectStore }) => {
+
+  const updateState = createState({
+    counter: 1,
+    input: ''
+  });
+
+  connectStore({ Auth });
+  connectStore({ VAT });
+
+  const onClick = () => {
+    updateState(({ state }) => state.counter++);
+  };
+
+  const removeSubscriber = (subscriber) => () => Channel.removeSubscriberById(subscriber.id);
+
+  return ({ attrs, state, Auth, Channel }) => {
+    const { user } = Auth;
+    return (
+      <div dynamicAttr={ attrs.dynamicAttr }>
+        <p class="static">Hello, { user.name }</p>
+        <p>You clicked { state.count } times!</p>
+        <button onclick={ onClick }>Click me!!!</button>
+        { $.and(user.hasSubscribers, (
+          <div class="subscribers">
+            Whatever, { () => user.name }
+            <ul>
+              { Channel.subscribers.map( subscriber => {
+                return (
+                  <li key={ subscriber.id }>
+                    <span>{ subscriber.name }</span>
+                    <button onClick={ removeSubscriber(subscriber) }>Remove</button>
+                  </li>
+                ); }
+              ) }
+            </ul>
+          </div>
+        ) }
+        { $.if(
+          state.someCondition,
+          <Component1 user={ user } />,
+          <Component2 user={ user } />
+        ) }
+      </div>
+    );
+  };
+};
+
+const map = (arr, fn) => {
+  let cacheArr, result;
+  return () => {
+    const a = arr();
+    if (a !== cacheArr){
+      result = arr.map(fn);
+      cacheArr = a;
+    }
+    return result;
+  };
+};
+
+const lift = (v, args) => typeof v === 'function' ? v.apply(null, args) : v;
+const $and = (x, y) => (...args) => lift(x, args) && lift(y, args);
+const $or = (x, y) => (...args) => lift(x, args) || lift(y, args);
+
+const $if = (x, y, z) => (...args) => lift(x, args) ? lift(y, args) : lift(z, args);
+
+const $if2 = (x, y, z) => {
+  let cache1, cache2, result;
+  return (...args) => {
+    const condition = lift(x, args);
+    if (condition) {
+      result = lift(y, args);
+      if result !== void 0;
+        cache1 = result;
+    } else {
+      result = lift(z, args);
+      if result !== void 0;
+        cache2 = result;
+    }
+    return result;
+  };
+};
+
+const $if3 = (condition, ...outcomes) => {
+  let result, cache = [];
+  return (...args) => {
+    const outcome = Number(!lift(x, args));
+    result = lift(outcomes[outcome], args);
+    if (result !== void 0) {
+      cache[outcome] = result;
+    }
+    return result;
+  };
+};
+
+const $add = (x, y) => () => lift(x) + lift(y);
+const $sub = (x, y) => () => lift(x) - lift(y);
+const $multiply = (x, y) => () => lift(x) * lift(y);
+const $divide = (x, y) => () => lift(x) / lift(y);
+const $pow = (x, y) => () => lift(x) ** lift(y);
+const $mod = (x, y) => () => lift(x) % lift(y);
